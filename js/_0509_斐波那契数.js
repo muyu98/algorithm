996935dcc476ce8f77bbb10e479e6e1a4e3d6f76
muyu <2359393914@qq.com>
2019-08-22 21:08:10 +0800

    罗马数字转整

--- js/_0509_斐波那契数.js
+++ js/_0509_斐波那契数.js
@@ -1,7 +1,14 @@
 /**
  * @param {number} N
  * @return {number}
- *
+ *通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+
+ F(0) = 0,   F(1) = 1
+ F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ 给定 N，计算 F(N)。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/fibonacci-number
  */
 var fib = function (N) {
     var a = 0, b = 1;
